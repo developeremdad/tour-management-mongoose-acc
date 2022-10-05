425c525f7437df24615b5d216dc2194544343b65
developeremdad <developeremdad@gmail.com>
2022-10-05 18:28:22 +0600

    connect mongodb altas with mongoose

--- server.js
+++ server.js
@@ -4,15 +4,32 @@ const colors = require("colors");
 
 const app = require("./app");
 
-// server
-const port = process.env.PORT || 8080;
-
-
-// database connection
-mongoose.connect(process.env.DATABASE,).then(() => {
-  console.log(`Database connection is successfully`.red.bold);
+// database connect with mongoose and mongodb atlas.
+const database = (module.exports = () =>{
+  const connectionParams = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  };
+  try {
+    mongoose.connect(process.env.URL,
+    connectionParams
+    );
+    console.log("Database Connection Successfully");
+  } catch (error) {
+    console.log("Database connection fail.", error);
+  }
 })
 
+database();
+
+
+
+// database connect with mongoose and local mongodb.
+// mongoose.connect(process.env.DATABASE,).then(() => {
+//   console.log(`Database connection is successfully`.red.bold);
+// })
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Tour Management app is running on port ${port}`.yellow.bold);
 });
